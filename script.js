01ed510c79fb9dba0eaf477353937baa939d7098
Hashinika Dilhari <161299267+Hashini316@users.noreply.github.com>
2024-04-15 18:52:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let menubar = document.querySelector('#menubar');
+let mynav = document.querySelector('.navbar');
+
+
+
+
+
+menubar.onclick = () =>{
+    menubar.classlist.toggle('fa-times')
+    mynav.classlist.toggle('active')
+}
